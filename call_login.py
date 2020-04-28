66ff809f8bb602afc9a561ffe70023a3859509ee
everyonepython <everyonepython@gmail.com>
2020-04-29 01:58:39 +0800

    Add：Login function separated from UI.

--- call_login.py
+++ call_login.py
@@ -8,6 +8,18 @@ class MyMainForm(QMainWindow, Ui_Form):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
+        self.login_pushButton.clicked.connect(self.display)  # 将 clicked 事件发送出去
+        self.quit_pushButton.clicked.connect(self.close)
+        self.password_lineEdit.returnPressed.connect(self.display)
+
+    def display(self):
+        username = self.username_lineEdit.text()
+        password = self.password_lineEdit.text()
+
+        if username == 'edc' and password == '123456':
+            self.user_textBrowser.setText(f'Welcome, {username}!')
+        else:
+            self.user_textBrowser.setText('You should try it again.')
 
 
 if __name__ == '__main__':
