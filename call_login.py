fd6988d2e847df8a9d48856c3c582709787754dd
everyonepython <everyonepython@gmail.com>
2020-04-29 02:57:51 +0800

    Add: ComboBox and OpenFileName

--- call_login.py
+++ call_login.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from ui_login import Ui_Form
 
 
@@ -11,6 +11,8 @@ class MyMainForm(QMainWindow, Ui_Form):
         self.login_pushButton.clicked.connect(self.display)  # 将 clicked 事件发送出去
         self.quit_pushButton.clicked.connect(self.close)
         self.password_lineEdit.returnPressed.connect(self.display)
+        self.comboBox.currentTextChanged.connect(self.choose_lang)
+        self.openfile_pushButton.clicked.connect(self.readfile)
 
     def display(self):
         username = self.username_lineEdit.text()
@@ -20,6 +22,17 @@ class MyMainForm(QMainWindow, Ui_Form):
             self.user_textBrowser.setText(f'Welcome, {username}!')
         else:
             self.user_textBrowser.setText('You should try it again.')
+
+    def choose_lang(self):
+        lang = self.comboBox.currentText()
+        self.user_textBrowser.setText(lang)
+
+    def readfile(self):
+        file_path, ok = QFileDialog.getOpenFileName(self,
+                                                    'Choose Single File',
+                                                    '/Users/maccair/',
+                                                    'All Files (*);;Text Files (*.txt)')
+        self.user_textBrowser.setText(file_path)
 
 
 if __name__ == '__main__':
